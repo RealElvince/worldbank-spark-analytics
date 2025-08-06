e2e5cf5dd82ac5300048fba1ec246b9bb1f1e36a
RealElvince <obuongely@yahoo.com>
2025-08-06 19:36:22 +0300

    sql function to run queries

--- runners/world_bank_analysis.py
+++ runners/world_bank_analysis.py
@@ -17,6 +17,27 @@ def run_sql_queries(query_file_path):
 
     # world bank view
     world_bank_data.createOrReplaceTemporaryView("world_bank_dataset")
+
+    # read and run sql queries
+    with open(query_file_path,"r") as file:
+        query = file.read()
+
+    query_result = spark.sql(query)
+    query_result.show(truncate=False)
+
+ # query file paths
+query_file_paths = [
+    "queries/unemployment_rate_per.sql",
+    "queries/top_5_gdp.sql",
+    "queries/lowest_electricity_access.sql",
+    "queries/co2_emission.sql",
+    "queries/average_life_expectancy.sql",
+]
+
+for path in query_file_paths:
+    run_sql_queries(path)
+
+
                         
 
 
