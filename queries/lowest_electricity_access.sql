647a568d365a23d2cd4e91f84c053588fa1c6ef8
RealElvince <obuongely@yahoo.com>
2025-08-06 19:02:17 +0300

    avg life expectancy

--- queries/lowest_electricity_access.sql
+++ queries/lowest_electricity_access.sql
@@ -1,4 +1,5 @@
-SELECT Country,Electricity_Access
-    FROM world_bank_dataset
-    ORDER BY Electricity_Access ASC
-    LIMIT 5
+SELECT 
+    Country,Electricity_Access
+ FROM world_bank_dataset
+ORDER BY Electricity_Access ASC
+LIMIT 5
