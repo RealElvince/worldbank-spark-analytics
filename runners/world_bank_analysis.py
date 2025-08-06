72b8adec939296f5fe14a911d3603600e2b4515b
RealElvince <obuongely@yahoo.com>
2025-08-06 19:18:34 +0300

    load csv data

---
+++ runners/world_bank_analysis.py
@@ -0,0 +1,19 @@
+from utils.spark_session import create_spark_session
+from schema.world_bank_schema import data_schema
+
+file_path = "data/world_bank_dataset.csv"
+
+spark = create_spark_session("Analysis")
+
+
+
+def run_sql_queries(query_file_path):
+    world_bank_data = spark.read.format("csv")\
+                        .option("header",True)\
+                        .schema(data_schema)\
+                        .load(file_path)
+    world_bank_data.show(10)
+    world_bank_data.printSchema()
+                        
+
+
